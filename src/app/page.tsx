7fe3ce4e67ca7154c36a3d3c79aa397c818af05b
Lucas Szimanski <lukasszimanskipadilha@gmail.com>
2026-08-27 23:28:27 -0400

    Initial LSGeo setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">LSGeo</h1>
+      <p className="mt-4 text-lg">Análise Geoespacial de Forma Simples.</p>
+    </main>
+  );
+}
